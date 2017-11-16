555d90b6d4d9293a27c599fe24090d95fe73310d
Favre Anael <anael.favre@gmail.com>
2017-11-16 13:30:37 +0100

    Rename helper

--- src/helpers.php
+++ src/helpers.php
@@ -2,8 +2,8 @@
 use Ifnot\LaravelVuex\Vuex\Store;
 use Illuminate\Database\Eloquent\Model;
 
-if(!function_exists('model_event')) {
-    function model_event(Model $model, $name, array $meta = [])
+if(!function_exists('model_mutation')) {
+    function model_mutation(Model $model, $name, array $meta = [])
     {
         $store = $model->store ? new $model->store($model) : new Store($model);
         $store->$name($model, $meta);
