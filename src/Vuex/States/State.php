5d6798b8c4213eb9ca7b8fcee2af2ce0775ac8b2
Favre Anael <anael.favre@gmail.com>
2017-12-06 16:45:33 +0100

    Allow channel to be callable

--- src/Vuex/States/State.php
+++ src/Vuex/States/State.php
@@ -14,35 +14,49 @@ class State
     protected $name;
     protected $channel;
 
-    public function __construct(Store $store, string $name, Channel $channel)
+    public function __construct(Store $store, string $name, $channel)
     {
         $this->store = $store;
         $this->name = $name;
         $this->channel = $channel;
     }
 
+    public function getChannel(Model $model, array $meta = [])
+    {
+        if(is_callable($this->channel)) {
+            return call_user_func_array($this->channel, [$model, $meta]);
+        } else {
+            return $this->channel;
+        }
+    }
+
     public function create(Model $model, array $meta = [])
     {
-        $this->emit($model, 'create', $this->channel, $meta);
+        $this->emit($model, 'create', $this->getChannel($model, $meta), $meta);
     }
 
     public function update(Model $model, array $meta = [])
     {
-        $this->emit($model, 'update', $this->channel, $meta);
+        $this->emit($model, 'update', $this->getChannel($model, $meta), $meta);
     }
 
     public function delete(Model $model, array $meta = [])
     {
-        $this->emit($model, 'delete', $this->channel,  $meta);
+        $this->emit($model, 'delete', $this->getChannel($model, $meta),  $meta);
     }
 
     public function __call($name, $args)
     {
-        $this->emit(Arr::get($args, 0), $name, $this->channel, Arr::get($args, 1, []));
+        $model = Arr::get($args, 0);
+        $meta = Arr::get($args, 1, []);
+
+        $this->emit($model, $name, $this->getChannel($model, $meta), $meta);
     }
 
-    public function emit(Model $model, string $event, Channel $channel, array $meta = [])
+    public function emit(Model $model, string $event, $channel, array $meta = [])
     {
-        event(new BroadcastEvent($this->store->getNamespace(), $this->name, $this->store->toArray($model), $event, $meta, $channel));
+        if($channel !== false) {
+            event(new BroadcastEvent($this->store->getNamespace(), $this->name, $this->store->toArray($model), $event, $meta, $channel));
+        }
     }
 }
--- src/Vuex/States/TunnelState.php
+++ src/Vuex/States/TunnelState.php
@@ -10,34 +10,12 @@ class TunnelState extends State
 {
     protected $channelHandler;
 
-    public function __construct(Store $store, $name, callable $channelHandler)
-    {
-        parent::__construct($store, $name, new Channel('global'));
-
-        $this->channelHandler = $channelHandler;
-    }
-
-    protected function getChannel(Model $model)
-    {
-        $channelHandler = $this->channelHandler;
-        return $channelHandler($model);
-    }
-
-    public function create(Model $model, array $meta = [])
-    {
-        $channel = $this->getChannel($model);
-
-        if ($channel !== false) {
-            $this->emit($model, 'create', $channel, $meta);
-        }
-    }
-
     public function update(Model $model, array $meta = [])
     {
         $original = (new $model)->fill($model->getOriginal());
 
-        $originalChannel = $this->getChannel($original);
-        $channel = $this->getChannel($model);
+        $originalChannel = $this->getChannel($original, []);
+        $channel = $this->getChannel($model, []);
 
         // If the model register a channel modification
         if ($originalChannel != $channel) {
@@ -54,15 +32,6 @@ class TunnelState extends State
         } elseif($channel !== false) {
             // If there is a channel, just update it
             $this->emit($model, 'update', $channel, $meta);
-        }
-    }
-
-    public function delete(Model $model, array $meta = [])
-    {
-        $channel = $this->getChannel($model);
-
-        if ($channel !== false) {
-            $this->emit($model, 'delete', $channel, $meta);
         }
     }
 }
